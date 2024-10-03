1b7126871d3dcb7f9aa6da2063a438bf2ecce6df
John-Anthony Graham <john-anthonygraham@John-Anthonys-MacBook-Air.local>
2024-10-03 23:44:36 +0100

    feat: use list to set up 2 teams each with captain

--- team_allocator.py
+++ team_allocator.py
@@ -1,1 +1,36 @@
-# Here I am just adding some generic text to check this works.
+import random
+
+# list of players 
+players = ["Martin", "Craig", "Sue", "Claire", "Dave", "Alice", 
+           "Sonakshi", "Harry", "Jack", "Rose", "Lexi", "Maria", 
+           "Thomas", "James", "William", "Ada", "Grace", "Jean", 
+           "Marissa", "Alan"]
+
+# Intro to tool
+print("Welcome to JAG's Team Allocator!")
+
+# loop 
+while True:
+
+    # shuffle the players listed 
+    random.shuffle(players)
+
+    # sort out and print team 1
+    team1 = players[:len(players)//2]
+    print("\nTEAM 1 \nCaptain:- " + random.choice(team1))
+    print("Full Team Line-up:- ")
+    for player in team1:
+        print(player)
+
+    # sort out and print team 2
+    team2 = players[len(players)//2:]
+    print("\nTEAM 2 \nCaptain:- " + random.choice(team2))
+    print("Full Team Line-up - ")
+    for player in team2:
+        print(player)
+        
+    response = input("\nPick teams again? (Type y or n ): ")
+    if response == "n":
+        print("\nThanks for using this tool, best of luck teams!")
+        break
+
