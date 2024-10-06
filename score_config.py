df59dfc913c37cecca61bdc59c79b284fd038cfe
John-Anthony Graham <John-Ant>
2024-10-06 13:31:40 +0100

    feat:adding comments into config code to improve transparency of code

--- score_config.py
+++ score_config.py
@@ -1,41 +1,67 @@
 # this file will contain all the code for various games for the games tracker. 
 
-def team_scoring():
+
+# generic scoring tool for one set of points 
+def scoring():
     while True: 
 
-        rounds_total = int(input("How many rounds do you intend to play?  \nTotal No. of Rounds: "))
+        # introduction to generic scoring application
+        print("\nThis application is designed to keep track of one team's scores\n\
+You will be asked how many rounds you wish to play.\n\
+Scores can then be recorded for each individual round. \n\
+The final score will then be displayed at the end.\n\n")
+ 
+        # how many rounds want to play 
+        rounds_total = int(input("How many rounds do you intend to play? \n\nTotal No. of Rounds: "))
         rounds_played = 0
         score = 0
 
+        # explains first round
         print("\nFIRST ROUND")
 
+        # loop to add points each round (with exception handling)
         while rounds_played != rounds_total:
-            num_to_add = int(input("\nHow many points to add this round? - "))
-            score = score + num_to_add
-            rounds_played += 1
-            print("\nROUND: " + str(rounds_played) + "\nScore is: " + str(score) + "\nRounds remaining: " + str(int(rounds_total) - int(rounds_played)))
+            try:
+                num_to_add = int(input("\nHow many points to add this round? - "))
+            except ValueError:
+                print("ERROR: Invalid input. Please enter a number.")
+                continue
 
+            # adds score given in round to total tracked score and moves rounds played by +1 
+            score += num_to_add
+            rounds_played += 1
+
+            # prints out game data each round (round on, current score, rounds remaining)
+            print("\nROUND: " + str(rounds_played) + "\nScore is: " + str(score) + "\nRounds remaining: " + str(rounds_total - rounds_played))
+
+            # code to end tracking if rounds played equals round to be played and print results 
             if rounds_played == rounds_total:
                 print("\n==========================\nFINAL SCORE IS: " + str(score) + "\n==========================\n")
                 again = input("Do you want to use this tracker again? (y/n)\n")
-           
+ 
+
+        # ask if want to play again with reset feature or application closure. 
         if again != "y":
-            print("\nThank you for using this scoring application")
-            break
+            print("\nThank you for using this scoring application\n\n")
+            quit()
         else:
             print("\n--------------------------\nNEW GAME\n")
             rounds_played == 0
 
 
-# Fun Facts 
+
+
+# Fun Facts - scoring for game called Fun Facts 
 def fun_facts():
     while True: 
         rounds_total = 8 
         rounds_played = 0
         score = 0
 
-        print("\nWelcome to fun facts!\n")
-              
+        # game intro 
+        print("\nWelcome to the Fun Facts score tracker!\n")
+
+        # how many players (with exception handling)
         players = input("How many people are going to be playing? (Minimum 4 players, Maximum 8 players)  ")
         try:
             players = int(players)
@@ -46,6 +72,7 @@ def fun_facts():
             print("ERROR: Invalid input. Please enter a number.")
             continue
 
+        # set up and rules explanation
         print("\n\nSETUP:\n\
         1. Shuffle the cards and place 8 facedown on the table.\n\
         2. Give each player a coloured arrow and the matching marker.\n\
@@ -65,20 +92,27 @@ GAME RULES:\n\
         Keep playing until all 8 cards have been used.\n\n\
 --------------------------\nFIRST ROUND")
 
+        # loop to add points each round (with exception handling)
         while rounds_played != rounds_total:
             try:
                 num_to_add = int(input("\nHow many points to add this round? - "))
             except ValueError:
                 print("ERROR: Invalid input. Please enter a number.")
                 continue
-
+                
+            # adds score given in round to total tracked score and moves rounds played by +1 
             score += num_to_add
             rounds_played += 1
+            
+            # prints out game data each round (round on, current score, rounds remaining)
             print("\nROUND: " + str(rounds_played) + "\nScore is: " + str(score) + "\nRounds remaining: " + str(rounds_total - rounds_played))
 
+
+            # code to end tracking if rounds played equals round to be played (in fun facts case 8 rounds) and print results 
             if rounds_played == rounds_total:
                 print("\n==========================\nFINAL SCORE IS: " + str(score) + "\n==========================\n")
 
+                # looping if/else if code to grade results as per game rules
                 # 4 player scoring 
                 if players == 4 and score <= 12:
                     print("Gotta start somewhere. Try again...")
@@ -136,6 +170,7 @@ GAME RULES:\n\
                     print("Perfect! There are no secrets between you - go team!")
                 
 
+                # ask if want to play again with reset feature or application closure. 
                 again = input("\n---------------------------\n\nDo you want to play again? (y/n)\n")
                 if again.lower() != "y":
                     print("\nThank you for using this scoring application\n\n")
